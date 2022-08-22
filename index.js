b48b2b3e9e2ff352068fe13f429b9e41773cc7ff
Ayush Jain <ayushshreeshreemal@gmail.com>
2022-08-21 21:59:12 -0700

    completed challenge 3

--- index.js
+++ index.js
@@ -5,19 +5,30 @@ function capitalize(str){
     str= str[0].toUpperCase() + str.slice(1)
     console.log('New - '+str)
 }
-capitalize('ayush')
+capitalize('ayush jain')
 console.log('------')
 
 // allCaps(): foo bar -> FOO BAR
 console.log('Challenge 2- allCaps()')
-
 function allCaps(str){
     console.log('Original - '+ str)
     str = str.toUpperCase();
     console.log('New - '+ str)
 }
-allCaps('ayush')
+allCaps('ayush jain')
 console.log('------')
 
 // capitalizeWords(): do all the things -> Do All The Things
-console.log('Challenge 2- capitalizeWords()')
+console.log('Challenge 3- capitalizeWords()')
+function capitalizeWords(str){
+    console.log('Original - '+str)
+    str= str.replace(str[0], str[0].toUpperCase())
+    length= str.length
+    for(i=0; i<=length; i= i+1){
+        if(str[i]== " "){
+            str = str.replace(str[i+1], str[i+1].toUpperCase())
+        }
+    }
+    console.log('New - '+str)
+}
+capitalizeWords('do all the things')
