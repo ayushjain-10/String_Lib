ee7ebafebfa36d7c5fa15cf7326e26d6703ee659
Ayush Jain <ayushshreeshreemal@gmail.com>
2022-08-21 20:08:45 -0700

    Completed challenge 2

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 // capitalize(): hello world -> Hello world
+console.log('Challenge 1- capitalize()') 
 function capitalize(str){
     console.log('Original - '+str)
     str= str[0].toUpperCase() + str.slice(1)
     console.log('New - '+str)
 }
 capitalize('ayush')
+console.log('------')
+
+// allCaps(): foo bar -> FOO BAR
+console.log('Challenge 2- allCaps()')
+
+function allCaps(str){
+    console.log('Original - '+ str)
+    str = str.toUpperCase();
+    console.log('New - '+ str)
+}
+allCaps('ayush')
+console.log('------')
+
+// capitalizeWords(): do all the things -> Do All The Things
+console.log('Challenge 2- capitalizeWords()')
